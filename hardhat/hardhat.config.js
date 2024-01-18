897056fedf1dcfcfcdc7aa6372b572f2f9fb54e6
Arshat Aitkozha <arshikoffice@gmail.com>
2024-01-18 17:41:25 +0600

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     mumbai: {
-      url: "https://still-silent-panorama.matic-testnet.quiknode.pro/a9a2dcee70c57492a9aebf8cf7878af025bcbbb1/",
-      accounts: ["c33747530beec1120ed3fdc89951bd8e77cfff09ade5543fdce84dd5a5d2a9e4"],
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
